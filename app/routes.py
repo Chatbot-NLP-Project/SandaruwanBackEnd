3ad25730961341b1de4ea4b5b563887dfeeedc14
Sandaruwan Kumarasinghe <sandaruwan.18@cse.mrt.ac.lk>
2021-09-22 13:01:27 +0530

    add-bot

---
+++ app/routes.py
@@ -0,0 +1,65 @@
+#from flask_mysqldb import MySQL,MySQLdb  
+
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask_cors import CORS
+from app import app
+from app import mysql
+#A Flask extension for handling Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
+#Axios doesnt work without this
+from app.src.Disease import predictDisease
+from app.src.HealthcareBot import chat1
+
+CORS(app)
+
+#######################################################
+##################''' Controllers '''##################
+#######################################################
+from app.src import User
+
+
+#######################################################
+##################''' User Routes '''##################
+#######################################################
+@app.route('/register', methods=["GET", "POST"]) 
+def register():
+    if request.method == 'GET':
+        return request.get_json()['email']
+    else:
+        return User.register(mysql)
+
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        return User.login(mysql)
+    else:
+        return "Done"
+
+@app.route('/logout')
+def logout():
+    return User.logout()
+     
+#######################################################
+################''' Telecom Routes '''#################
+#######################################################
+
+
+@app.route("/chat")
+def chat():
+    re = chat1("Hi")
+    return {"members":["Member",re]}
+
+@app.route("/reply",methods=["GET","POST"])
+def reply():
+    if request.method == 'POST':
+        # if request.get_json()['msg'] == "Hi":
+        re = chat1(request.get_json()['msg'])
+        print(re)
+        return {"members": re}
+
+@app.route("/predict",methods=["GET","POST"])
+def predict():
+    if request.method == 'POST':
+        # if request.get_json()['msg'] == "Hi":
+        re = predictDisease(request.get_json()['diseases'])
+        return {"members": re}
---
+++ app/src/Disease.py
@@ -0,0 +1,47 @@
+import joblib
+import numpy as np  # linear algebra
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+filename = 'model.pkl'
+
+rf = joblib.load(filename)
+combined_df = pd.read_csv('combined_df.csv')
+X = combined_df[['Symptom_1', 'Symptom_2', 'Symptom_3']]  # get symptom values
+P = X[:]
+le = LabelEncoder()
+
+
+def predictDisease(lst):
+    filename = 'model.pkl'
+
+    rf = joblib.load(filename)
+    combined_df = pd.read_csv('combined_df.csv')
+    X = combined_df[['Symptom_1', 'Symptom_2', 'Symptom_3']]  # get symptom values
+    P = X[:]
+    le = LabelEncoder()
+
+    d = {'Symptom_1': lst[0], 'Symptom_2': lst[1], 'Symptom_3': lst[2]}
+    P = P.append(d, ignore_index=True)
+    for lul in P.columns:
+        P[lul] = le.fit_transform(P[lul].astype(str))
+    z = P.tail(1)
+
+    # print(z)
+
+    df2 = pd.DataFrame(z)
+    y_pred = rf.predict(df2)
+    k = rf.predict_proba(df2)
+    prob = np.max(k, axis=1)
+    if (prob > 0.7):
+        return y_pred[0]
+    else:
+        # print(prob)
+        # print('disease : ' + y_pred[0])
+        n = 0
+        return 'Re-enter correct symptoms........... or priorities symptoms'
+
+
+# print(lst)
+
+A = ['itching', 'skin_rash', 'nodal_skin_eruptions']
