aab9d3e4c770b6c45b276fd3ff069e3d7370447d
Sandaruwan Kumarasinghe <sandaruwan.18@cse.mrt.ac.lk>
2021-10-22 12:20:56 +0530

    change chatbot

--- app/src/HealthcareBot.py
+++ app/src/HealthcareBot.py
@@ -1,3 +1,4 @@
+   
 from app import routes
 from app import db
 
@@ -10,15 +11,12 @@ from tensorflow.python.framework import ops
 import random
 import json
 import pickle
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-
-
 
 stemmer = LancasterStemmer()
 
 with open("intents.json") as file:
     data = json.load(file)
+
 
 try:
     with open("data.pickle", "rb") as f:
@@ -39,7 +37,7 @@ except:
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
 
-    words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in "?"]
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
     words = sorted(list(set(words)))
     labels = sorted(labels)
 
@@ -50,7 +48,7 @@ except:
 
     for x, doc in enumerate(docs_x):
         bag = []
-        wrds = [lemmatizer.lemmatize(w) for w in doc]
+        wrds = [stemmer.stem(w) for w in doc]
 
         for w in words:
             if w in wrds:
@@ -65,9 +63,10 @@ except:
         output.append(output_row)
     training = numpy.array(training)
     output = numpy.array(output)
+
     #
-    # with open("data.pickle", "wb") as f:
-    #     pickle.dump((words, labels, training, output), f)
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 ops.reset_default_graph()
 
@@ -78,15 +77,18 @@ net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+
+# model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 # model.save("model.tflearn")
 
-# model.load("model.tflearn")
+
+model.load("model.tflearn")
+
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
 
     s_words = nltk.word_tokenize(s)
-    s_words = [lemmatizer.lemmatize(word.lower()) for word in s_words]
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
 
     for se in s_words:
         for i, w in enumerate(words):
@@ -96,24 +98,28 @@ def bag_of_words(s, words):
     return numpy.array(bag)
 
 
-def chat1(inp):
+def chat(inp):
     # print("Start talking with bot")
     # while True:
     #     inp = input("You: ")
     #     if inp.lower() == "quit":
     #         break
 
-
-    results = model.predict([bag_of_words(inp, words)])
+    results = model.predict([bag_of_words(inp, words)])[0]
     results_index = numpy.argmax(results)
     tag = labels[results_index]
     # if tag == "predict":
     #     return "predict"
     # else:
-    res = ""
-    for tg in data["intents"]:
-        if tg["tag"] == tag:
-            responses = tg["responses"]
-            # print(responses)
-            res = random.choice(responses)
+    # res = ""
+    # print(type(results))
+    # print(results[0][results_index])
+    if results[results_index] > 0.7:
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg["responses"]
+                print(responses)
+                res = random.choice(responses)
+    else:
+        res = "I didn't understand it"
     return res
